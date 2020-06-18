0483bbe97f3e2095964f4c314123d8b29f11e16f
Khadija <khadija.genius@gmail.com>
2020-06-18 12:46:49 +0530

    Fixed clobbers

--- src/ios/MathCalculator.m
+++ src/ios/MathCalculator.m
@@ -17,7 +17,8 @@
     CDVPluginResult* pluginResult = nil;
     NSString* param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
     NSString* param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-    if(param1 >= 0 && param2 ){
+    if(param1 >=0 && param2 >= 0) // changed here
+    {
         NSString* total = @"100";
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
     }else {
