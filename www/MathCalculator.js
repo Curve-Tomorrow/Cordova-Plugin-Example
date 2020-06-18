554cae11ade9fe44dff1df6b82813faa400a987c
Khadija <khadija.genius@gmail.com>
2020-06-18 15:12:45 +0530

    Fix subtract

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -4,6 +4,6 @@ module.exports.add = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'add', [arg0]);
 };
 
-module.exports.substract = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'substract', [arg0]);
+module.exports.subtract = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'subtract', [arg0]);
 };
